453e25bcc11655a56ffbbf57bbb295942d3f2aa4
flyleaf <flyleaf91@163.com>
2019-02-28 15:33:06 +0800

    提交之前的测试代码及下载算法

---
+++ 8-EXTI—外部中断/User/main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32h7xx_hal.h"
+
+static void SystemClock_Config(void);
+#define IMAGE_WIDTH             (100U)
+#define IMAGE_HEIGHT            (100U)
+#endif /* __MAIN_H */
+
