765fed4d5683177c8b038e1f114b13cd56650b87
LONG <1558193230@qq.com>
2019-04-30 12:08:32 +0800

    改一下测试方法

--- SDMMC—SD卡读写测试/User/main.c
+++ SDMMC—SD卡读写测试/User/main.c
@@ -48,20 +48,19 @@ int main(void)
     DEBUG_USART_Config();	
     /* 初始化独立按键 */
     Key_GPIO_Config();
-    printf("\r\n欢迎使用野火  STM32 H743 开发板。\r\n");	
+    printf("\r\n欢迎使用野火  STM32 H750 开发板。\r\n");	
     printf("在开始进行SD卡基本测试前，请给开发板插入32G以内的SD卡\r\n");			
     printf("本程序会对SD卡进行非文件系统方式读写，会删除SD卡的文件系统\r\n");		
     printf("实验后可通过电脑格式化或使用SD卡文件系统的例程恢复SD卡文件系统\r\n");		
     printf("\r\n 但sd卡内的原文件不可恢复，实验前务必备份SD卡内的原文件！！！\r\n");		
     printf("\r\n 若已确认，请按开发板的KEY1按键，开始SD卡测试实验....\r\n");	  
   
+    while(Key_Scan(KEY1_GPIO_PORT,KEY1_PIN) == KEY_OFF);
+    printf("\r\n开始进行SD卡读写实验\r\n");	
+    SD_Test();	
     while(1)
     {
-        if(	Key_Scan(KEY1_GPIO_PORT,KEY1_PIN) == KEY_ON)
-        {
-            printf("\r\n开始进行SD卡读写实验\r\n");	
-            SD_Test();			            
-        }      
+            
     }			
 }
 /**
