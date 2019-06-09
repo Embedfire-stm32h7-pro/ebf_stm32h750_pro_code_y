7ed0ca9046eae9790eb13911406cbe3c717b10bc
zzz_xxj <995007786@qq.com>
2019-06-09 14:44:22 +0800

    修改注释 2017-xx-xx -> 2019-xx-xx

--- PWR—电源管理/PWR—PVD监控/User/pvd/bsp_pvd.c
+++ PWR—电源管理/PWR—PVD监控/User/pvd/bsp_pvd.c
@@ -3,7 +3,7 @@
   * @file    bsp_pvd.c
   * @author  fire
   * @version V1.0
-  * @date    2017-xx-xx
+  * @date    2019-xx-xx
   * @brief   pvdº‡øÿ≈‰÷√
   ******************************************************************************
   * @attention
