25bd3a93cb8a2da3d091f2a3a1354b3b94b89cca
jc <798025823@qq.com>
2020-06-15 18:06:05 +0800

    修改串口为串口2

--- 12-USART一串口通信/USART2—RS232接发/User/led/bsp_led.c
+++ 12-USART一串口通信/USART2—RS232接发/User/led/bsp_led.c
@@ -8,7 +8,7 @@
   ******************************************************************
   * @attention
   *
-  * 实验平台:野火 STM32H750开发板 
+  * 实验平台:野火 STM32H750 开发板 
   * 论坛    :http://www.firebbs.cn
   * 淘宝    :http://firestm32.taobao.com
   *
@@ -31,7 +31,6 @@ void LED_GPIO_Config(void)
 	LED1_GPIO_CLK_ENABLE();
 	LED2_GPIO_CLK_ENABLE();
 	LED3_GPIO_CLK_ENABLE();
-//	LED4_GPIO_CLK_ENABLE();
 
 	/*选择要控制的GPIO引脚*/															   
 	GPIO_InitStruct.Pin = LED1_PIN;	
@@ -56,9 +55,7 @@ void LED_GPIO_Config(void)
 	GPIO_InitStruct.Pin = LED3_PIN;	
 	HAL_GPIO_Init(LED3_GPIO_PORT, &GPIO_InitStruct);	
 
-
 	/*关闭RGB灯*/
-	LED_RGBOFF;
-		
+	LED_RGBOFF;	
 }
 /*********************************************END OF FILE**********************/
